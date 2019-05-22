d4388b145eca99479a6bf6cb66cfb8dcccdb64c3
JPunnar <juri.punnar@gmail.com>
2019-05-23 00:08:09 +0300

    new stuff

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -5,3 +5,18 @@ $(function() {
 function clearChatInput() {
   $(this).find('input[type="text"]').val('');
 }
+
+// $(document).ready(function(){
+//   $('#new_chat_message').on('ajax:success', function(e, data, status, xhr){
+//     alert("donez")
+//     console.log("ajax done");
+//     console.log(e);
+//     console.log(data);
+//     console.log(status);
+//     console.log(xhr);
+//   });
+// });
+
+$('#new_chat_message').ajaxSuccess(function( event, xhr ) {
+  console.log(xhr.responseText);
+});
--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,6 +1,8 @@
 class ChatMessagesController < ApplicationController
   def create
     ChatMessage.create!(message: params[:chat_message][:message])
-
+    @chat_messages = ChatMessage.all
+    @chat_message = ChatMessage.new
+    render partial: 'chat/chat'
   end
 end
